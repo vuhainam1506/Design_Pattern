70c48af711900f5655be741e07f3f68adfb30385
caubebutchii <caubebutchi2k3@gmail.com>
2025-03-11 17:00:05 +0700

    sua lai product va cap nhat minh chung

--- BaiTap_DP_Composite/src/Main.java
+++ BaiTap_DP_Composite/src/Main.java
@@ -3,11 +3,15 @@ public class Main {
 
 
         Component table = new Table();
-        Component product1 = new Product("Chair", 10.333, 2);
-        Component product2 = new Product("Desk", 20, 1);
+        Component product1 = new Product("Chair", 20.111);
+        Component product2 = new Product("Desk", 10.9);
+        Component product3 = new Product("Coffee", 5.0);
+        Component product4 = new Product("Tea", 3.0);
 
         ((Table) table).addProduct(product1);
         ((Table) table).addProduct(product2);
+        ((Table) table).addProduct(product3);
+        ((Table) table).addProduct(product4);
 
         System.out.println(table.getAtts());
         System.out.println("Total Price: " + table.getTotalPrice());
--- BaiTap_DP_Composite/src/Product.java
+++ BaiTap_DP_Composite/src/Product.java
@@ -1,19 +1,17 @@
 public class Product implements Component{
     private String name;
     private double price;
-    private int quantity;
 
-    public Product(String name, double price, int quantity) {
+    public Product(String name, double price) {
         this.name = name;
         this.price = price;
-        this.quantity = quantity;
     }
 
     public String getAtts() {
-        return "Product: " + name + ", Price: " + price + ", Quantity: " + quantity;
+        return "Product: " + name + ", Price: " + price;
     }
 
     public double getTotalPrice() {
-        return price * quantity;
+        return price;
     }
 }
